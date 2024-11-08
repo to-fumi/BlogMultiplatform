96d5d18e187cf32900444b687ed1f2d0f2f1bdf5
tofuminto <tototofumifumin@gmail.com>
2024-11-09 00:27:15 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+    }
+}
+
+rootProject.name = "blogmultiplatform"
+
+include(":site")
+include(":worker")
