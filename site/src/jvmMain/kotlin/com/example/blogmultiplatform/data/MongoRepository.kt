d4dbe77f11eafdc6052d82fc87f5affa2469ef08
to-fumi <tototofumifumin@gmail.com>
2024-11-15 14:40:57 +0900

    jvm/add-usercheck-api

--- site/src/jvmMain/kotlin/com/example/blogmultiplatform/data/MongoRepository.kt
+++ site/src/jvmMain/kotlin/com/example/blogmultiplatform/data/MongoRepository.kt
@@ -3,5 +3,5 @@ package com.example.blogmultiplatform.data
 import com.example.blogmultiplatform.models.User
 
 interface MongoRepository {
-    suspend fun userCheckExistence(user: User): User?
+    suspend fun checkUserExistence(user: User): User?
 }
