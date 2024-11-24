b7ae61d5a76af7eb607b67ba6ef8c32cfba40f3c
Fumiya Toyama <66166495+to-fumi@users.noreply.github.com>
2024-11-24 20:14:57 +0900

    js/create-loadindicator (#21)

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/util/ApiFunctions.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/util/ApiFunctions.kt
@@ -1,11 +1,19 @@
 package com.example.blogmultiplatform.util
 
+import com.example.blogmultiplatform.components.LoadingIndicator
+import com.example.blogmultiplatform.models.RandomJoke
 import com.example.blogmultiplatform.models.User
 import com.example.blogmultiplatform.models.UserWithoutPassword
+import com.example.blogmultiplatform.util.Constants.HUMOR_API_URL
 import com.varabyte.kobweb.browser.api
+import com.varabyte.kobweb.browser.http.http
+import kotlinx.browser.localStorage
 import kotlinx.browser.window
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import org.w3c.dom.get
+import org.w3c.dom.set
+import kotlin.js.Date
 
 suspend fun checkUserExistence(user: User): UserWithoutPassword? {
     return try {
@@ -30,5 +38,42 @@ suspend fun checkUserId(id: String): Boolean {
     } catch (e: Exception) {
         println(e.message.toString())
         false
+    }
+}
+
+suspend fun fetchRandomJoke(onComplete: (RandomJoke) -> Unit) {
+    val date = localStorage["date"]
+    if (date != null) {
+        val difference = (Date.now() - date.toDouble())
+        val dayHasPassed = difference >= 86400000
+        if(dayHasPassed) {
+            try {
+                val result = window.http.get(HUMOR_API_URL).decodeToString()
+                onComplete(Json.decodeFromString(result))
+                localStorage["date"] = Date.now().toString()
+                localStorage["joke"] = result
+            } catch (e: Exception) {
+                onComplete(RandomJoke(id = -1, joke = e.message.toString()))
+                println(e.message)
+            }
+        } else {
+            try {
+                localStorage["joke"]?.let { Json.decodeFromString<RandomJoke>(it) }
+                    ?.let { onComplete(it) }
+            } catch (e: Exception) {
+                onComplete(RandomJoke(id = -1, joke = e.message.toString()))
+                println(e.message)
+            }
+        }
+    } else {
+        try {
+            val result = window.http.get(HUMOR_API_URL).decodeToString()
+            onComplete(Json.decodeFromString(result))
+            localStorage["date"] = Date.now().toString()
+            localStorage["joke"] = result
+        } catch (e: Exception) {
+            onComplete(RandomJoke(id = -1, joke = e.message.toString()))
+            println(e.message)
+        }
     }
 }
