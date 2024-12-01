adcbf3bac0d0bee3b29783ecf91ed0ee3f6ed657
Fumiya Toyama <66166495+to-fumi@users.noreply.github.com>
2024-12-01 18:56:44 +0900

    jvm/add-new-post (#32)

---
+++ site/src/jvmMain/kotlin/com/example/blogmultiplatform/api/Posts.kt
@@ -0,0 +1,26 @@
+package com.example.blogmultiplatform.api
+
+import com.example.blogmultiplatform.data.MongoDB
+import com.example.blogmultiplatform.models.Post
+import com.varabyte.kobweb.api.Api
+import com.varabyte.kobweb.api.ApiContext
+import com.varabyte.kobweb.api.data.getValue
+import com.varabyte.kobweb.api.http.setBodyText
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.encodeToString
+import org.bson.codecs.ObjectIdGenerator
+
+@Api(routeOverride = "addpost")
+suspend fun addPost(context: ApiContext) {
+    try {
+        val post = context.req.body?.decodeToString()?.let { Json.decodeFromString<Post>(it) }
+        val newPost = post?.copy(id = ObjectIdGenerator().generate().toString())
+        context.res.setBodyText(
+            newPost?.let {
+                context.data.getValue<MongoDB>().addPost(it).toString()
+            } ?: false.toString()
+        )
+    } catch (e: Exception) {
+        context.res.setBodyText(Json.encodeToString(e.message))
+    }
+}
