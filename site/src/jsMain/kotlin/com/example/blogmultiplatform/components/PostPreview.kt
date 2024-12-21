de4795e940980ca9de5ebb73277dd80d8ed37aa7
Fumiya Toyama <66166495+to-fumi@users.noreply.github.com>
2024-12-22 01:23:44 +0900

    js/create-a-postsection (#72)

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/components/PostPreview.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/components/PostPreview.kt
@@ -15,11 +15,9 @@ import com.varabyte.kobweb.compose.css.Cursor
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.ObjectFit
 import com.varabyte.kobweb.compose.css.Overflow
-import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.css.TextOverflow
 import com.varabyte.kobweb.compose.css.Transition
 import com.varabyte.kobweb.compose.css.TransitionProperty
-import com.varabyte.kobweb.compose.css.Visibility
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
@@ -41,19 +39,14 @@ import com.varabyte.kobweb.compose.ui.modifiers.onClick
 import com.varabyte.kobweb.compose.ui.modifiers.overflow
 import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.modifiers.size
-import com.varabyte.kobweb.compose.ui.modifiers.textAlign
 import com.varabyte.kobweb.compose.ui.modifiers.textOverflow
 import com.varabyte.kobweb.compose.ui.modifiers.transition
-import com.varabyte.kobweb.compose.ui.modifiers.visibility
 import com.varabyte.kobweb.compose.ui.styleModifier
 import com.varabyte.kobweb.compose.ui.thenIf
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.rememberPageContext
 import com.varabyte.kobweb.silk.components.graphics.Image
-import com.varabyte.kobweb.silk.components.layout.SimpleGrid
-import com.varabyte.kobweb.silk.components.layout.numColumns
 import com.varabyte.kobweb.silk.components.text.SpanText
-import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import org.jetbrains.compose.web.css.CSSSizeValue
 import org.jetbrains.compose.web.css.CSSUnit
 import org.jetbrains.compose.web.css.LineStyle
@@ -74,6 +67,7 @@ fun PostPreview(
     titleMaxLines: Int = 2,
     onSelect: (String) -> Unit = {},
     onDeselect: (String) -> Unit = {},
+    onClick: (String) -> Unit = {},
 ) {
     val context = rememberPageContext()
     var checked by remember(selectableMode) { mutableStateOf(false) }
@@ -99,7 +93,7 @@ fun PostPreview(
                             onDeselect(post.id)
                         }
                     } else {
-                        context.router.navigateTo(Screen.AdminCreate.passPostId(id = post.id))
+                        onClick(post.id)
                     }
                 }
                 .transition(
@@ -224,50 +218,5 @@ fun PostContent(
                 )
             }
         }
-    }
-}
-
-@Composable
-fun Posts(
-    breakpoint: Breakpoint,
-    posts: List<PostWithoutDetails>,
-    selectableMode: Boolean = false,
-    onSelect: (String) -> Unit,
-    onDeselect: (String) -> Unit,
-    showMoreVisibility: Boolean,
-    onShowMore: () -> Unit,
-) {
-    Column(
-        modifier = Modifier.fillMaxWidth(
-            if(breakpoint > Breakpoint.MD) 80.percent else 90.percent
-        ),
-        verticalArrangement = Arrangement.Center,
-    ) {
-        SimpleGrid(
-            modifier = Modifier.fillMaxWidth(),
-            numColumns = numColumns(base = 1, sm = 2, md = 3, lg = 4)
-        ) {
-            posts.forEach {
-                PostPreview(
-                    post = it,
-                    selectableMode = selectableMode,
-                    onSelect = onSelect,
-                    onDeselect = onDeselect,
-                )
-            }
-        }
-        SpanText(
-            modifier = Modifier
-                .fillMaxWidth()
-                .margin(topBottom = 50.px)
-                .textAlign(TextAlign.Center)
-                .fontFamily(FONT_FAMILY)
-                .fontSize(16.px)
-                .fontWeight(FontWeight.Medium)
-                .cursor(Cursor.Pointer)
-                .visibility(if (showMoreVisibility) Visibility.Visible else Visibility.Hidden)
-                .onClick { onShowMore() },
-            text = "Show more"
-        )
     }
 }
