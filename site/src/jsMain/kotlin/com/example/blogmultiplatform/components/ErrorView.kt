2872eec674a186c2c8d03ae4fbe07cc9fcc0f049
to-fumi <tototofumifumin@gmail.com>
2024-12-25 00:03:44 +0900

    common/fix-displaying-code-and-fix-position-on-Errorpage

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/components/ErrorView.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/components/ErrorView.kt
@@ -7,7 +7,6 @@ import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
 import com.varabyte.kobweb.compose.ui.modifiers.fontSize
 import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
