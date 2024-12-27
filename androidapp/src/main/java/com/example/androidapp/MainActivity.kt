201a1c0b05e04910c4e524f111e2e8536f632e0c
Fumiya Toyama <66166495+to-fumi@users.noreply.github.com>
2024-12-27 13:53:19 +0900

    android/navigation-setup (#94)
    * android/navigation-setup
    
    * androind/fix

--- androidapp/src/main/java/com/example/androidapp/MainActivity.kt
+++ androidapp/src/main/java/com/example/androidapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.example.androidapp.navigation.SetupNavGraph
 import com.example.androidapp.ui.theme.BlogmultiplatformTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +14,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BlogmultiplatformTheme {
-
+                val navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
