15bcfa0631f91995377dea40437f3331efaeea7f
to-fumi <tototofumifumin@gmail.com>
2025-01-04 02:35:38 +0900

    android/navgraph-cleanup-and-fix-build

---
+++ androidapp/src/main/java/com/example/androidapp/navigation/destinations/Home.kt
@@ -0,0 +1,46 @@
+package com.example.androidapp.navigation.destinations
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.androidapp.models.Category
+import com.example.androidapp.navigation.Screen
+import com.example.androidapp.screens.home.HomeScreen
+import com.example.androidapp.screens.home.HomeViewModel
+
+fun NavGraphBuilder.homeRoute(
+    onCategorySelect: (Category) -> Unit,
+    onPostClick: (String) -> Unit,
+) {
+    composable(route = Screen.Home.route) {
+        val viewModel: HomeViewModel = viewModel()
+        var query by remember { mutableStateOf("") }
+        var searchBarOpened by remember { mutableStateOf(false) }
+        var active by remember { mutableStateOf(false) }
+
+        HomeScreen(
+            posts = viewModel.allPosts.value,
+            searchedPosts = viewModel.searchPosts.value,
+            query = query,
+            searchBarOpened = searchBarOpened,
+            active = active,
+            onActiveChange = { active = it },
+            onQueryChange = { query = it },
+            onCategorySelect = onCategorySelect,
+            onSearchBarChange = { opened ->
+                searchBarOpened = opened
+                if (!opened) {
+                    query = ""
+                    active = false
+                    viewModel.resetSearchedPosts()
+                }
+            },
+            onSearch = viewModel::searchPostsByTitle,
+            onPostClick = onPostClick,
+        )
+    }
+}
