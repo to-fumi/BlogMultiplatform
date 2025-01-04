74f5fdf36930f6769420e4fc6df433654a5e6274
Fumiya Toyama <66166495+to-fumi@users.noreply.github.com>
2025-01-04 13:04:47 +0900

    js/add-box-shadow (#111)

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/styles/PostPreviewStyle.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/styles/PostPreviewStyle.kt
@@ -4,6 +4,7 @@ import com.example.blogmultiplatform.models.Theme
 import com.varabyte.kobweb.compose.css.Transition
 import com.varabyte.kobweb.compose.css.TransitionProperty
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.boxShadow
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.scale
 import com.varabyte.kobweb.compose.ui.modifiers.transition
@@ -12,6 +13,7 @@ import com.varabyte.kobweb.silk.style.selectors.hover
 import org.jetbrains.compose.web.css.ms
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.rgba
 
 val PostPreviewStyle = CssStyle {
     base {
@@ -28,6 +30,39 @@ val PostPreviewStyle = CssStyle {
     }
     hover {
         Modifier
+            .boxShadow(
+                offsetX = 0.px,
+                offsetY = 0.px,
+                blurRadius = 8.px,
+                spreadRadius = 5.px,
+                color = rgba(0, 0, 0, 0.06),
+            )
+            .scale(102.percent)
+    }
+}
+
+val MainPostPreviewStyle = CssStyle {
+    base {
+        Modifier
+            .scale(100.percent)
+            .transition(
+                Transition.of(
+                    property = TransitionProperty.All,
+                    duration = 100.ms,
+                    timingFunction = null,
+                    delay = null
+                )
+            )
+    }
+    hover {
+        Modifier
+            .boxShadow(
+                offsetX = 0.px,
+                offsetY = 0.px,
+                blurRadius = 8.px,
+                spreadRadius = 5.px,
+                color = rgba(0, 162, 255, 0.06),
+            )
             .scale(102.percent)
     }
 }
